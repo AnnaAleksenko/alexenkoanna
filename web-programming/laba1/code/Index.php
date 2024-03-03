f6d3befe732647e8dccaea94df8dab825685823c
AnnaAleksenko <113089651+AnnaAleksenko@users.noreply.github.com>
2024-03-03 22:05:50 +0200

    Add integer, non-integer value, differences between expenses
    ( Numbers)

--- web-programming/laba1/code/Index.php
+++ web-programming/laba1/code/Index.php
@@ -10,4 +10,24 @@ $order = &$very_bad_unclear_name;
 $order .= " and  large fries";
 
 // Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
+echo "\nYour order is: $very_bad_unclear_name.". "<br>" ;
+
+/* №2 */
+$integerVar = 21;
+echo $integerVar. "<br>" ;
+
+
+echo "\n";
+
+
+$floatVar = 3.1415926535;
+echo $floatVar. "<br>" ;
+
+
+echo 10 + 2 . "<br>" ;
+
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+$expenses_difference = $last_month - $this_month;
+echo  "\n Разницы между расходами в прошлом месяце и в этом месяце составила: $expenses_difference". "<br>" ;
