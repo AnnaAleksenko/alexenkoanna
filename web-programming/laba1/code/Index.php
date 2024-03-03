22dd219b694d6505ea7064fb6d258de5baaa8ba1
Anna Aleksenko <aleksenko9@mail.ru>
2024-03-03 22:24:49 +0200

    Added multiplication for $days and division for $days_per_language
    (multiplication and division)

--- web-programming/laba1/code/Index.php
+++ web-programming/laba1/code/Index.php
@@ -10,24 +10,38 @@ $order = &$very_bad_unclear_name;
 $order .= " and  large fries";
 
 // Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.". "<br>" ;
+echo "\nYour order is: $very_bad_unclear_name.";
 
 /* №2 */
 $integerVar = 21;
-echo $integerVar. "<br>" ;
+echo $integerVar;
 
 
 echo "\n";
 
 
 $floatVar = 3.1415926535;
-echo $floatVar. "<br>" ;
+echo $floatVar;
 
 
-echo 10 + 2 . "<br>" ;
+echo 10 + 2;
 
 
 $last_month = 1187.23;
 $this_month = 1089.98;
 $expenses_difference = $last_month - $this_month;
-echo  "\n Разницы между расходами в прошлом месяце и в этом месяце составила: $expenses_difference". "<br>" ;
+echo  "\n Разницы между расходами в прошлом месяце и в этом месяце составила: $expenses_difference";
+
+/* №11 multiplication and division*/
+
+$num_languages = 4;
+
+
+$months = 11;
+
+$days = $months * 16;
+
+
+$days_per_language = $days / $num_languages;
+
+echo "\n  Количество дней которое в среднем у нее ушло на изучение каждого языка: $days_per_language";
