76404cf4baf1a595321f278e35998b14a98cae5f
Anna Aleksenko <aleksenko9@mail.ru>
2024-05-03 01:14:30 +0200

    added working with google sheets in alex4

--- web-programming/laba5(4)/code/alex4.php
+++ web-programming/laba5(4)/code/alex4.php
@@ -41,7 +41,47 @@
         </tr>
         </thead>
         <tbody>
+        <?php
+        require __DIR__ . '/vendor/autoload.php'; 
+
+        $client = new \Google_Client();
+        $client->setApplicationName('The table');
+        $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
+        $client->setAccessType('offline');
+        $path = __DIR__ . '/credentials.json';
+        $client->setAuthConfig($path);
+
         
+        $service = new Google\Service\Sheets($client);
+
+        
+        $spreadsheetId = '1uwQI4ppiPoQWETs3BOu_1hsBcfc-Ll6l5zhWyZI1I_g';
+
+        
+        $range = 'Sheet1!A:D';
+
+        $service = new Google_Service_Sheets($client);
+        $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+        $values = $response->getValues();
+
+        
+        if (!empty($values)) {
+            $isFirstRow = true; // Флаг для определения первой строки
+            foreach ($values as $row) {
+                if ($isFirstRow) {
+                    $isFirstRow = false;
+                    continue; // Пропускаем первую строку
+                }
+                echo '<tr>';
+                foreach ($row as $cell) {
+                    echo '<td>' . htmlspecialchars($cell) . '</td>';
+                }
+                echo '</tr>';
+            }
+        } else {
+            echo 'Пусто.';
+        }
+        ?>
         </tbody>
     </table>
 </div>
