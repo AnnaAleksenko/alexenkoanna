53b9fc1331bc773d0e6aca28a21298719a4196b7
Anna Aleksenko <aleksenko9@mail.ru>
2024-09-13 17:24:29 +0200

    edited second time 1.2 laba1

--- СРМ/Laba 1/1.2/1.2/1.2.cpp
+++ СРМ/Laba 1/1.2/1.2/1.2.cpp
@@ -6,21 +6,20 @@ using namespace std;
 int main()
 {
 	int a = 10;
-	int i;
-	int b = 1;
-	int j;
-	int c = pow(10, 7);
+	int b = 1;  
+	int j;     
+	int c = pow(10, 7); //количество 
+
 	clock_t start = clock();
-	for (j = 1; j <= c; j++)
-	{
-		for (i = 1; i <= a; i++)
-		{
-			b = b * i;
+	for (j = 1; j <= c; j++) {
+		b = 1; // для нового расчета факториала
+		for (int i = 1; i <= a; i++) {
+			b *= i; //  факториал
 		}
-		b = 1;
 	}
 	clock_t end = clock();
 	double seconds = (double)(end - start) / CLOCKS_PER_SEC;
 	cout << fixed << setprecision(100) << seconds;
 }
+
 
