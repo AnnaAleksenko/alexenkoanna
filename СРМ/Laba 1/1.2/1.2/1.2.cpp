ca4b06a3a1af53d7cabbd095ef9910a874187d8f
Anna Aleksenko <aleksenko9@mail.ru>
2024-09-13 17:12:56 +0200

    1.2

---
+++ СРМ/Laba 1/1.2/1.2/1.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <time.h>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+#include <time.h>
+	int a = 10;
+	int i;
+	int b = 1;
+	int j;
+	int c = pow(10, 7);
+	clock_t start = clock();
+	for (j = 1; j <= c; j++)
+	{
+		for (i = 1; i <= a; i++)
+		{
+			b = b * i;
+		}
+		b = 1;
+	}
+	clock_t end = clock();
+	double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+	cout << fixed << setprecision(100) << seconds;
+}
+
